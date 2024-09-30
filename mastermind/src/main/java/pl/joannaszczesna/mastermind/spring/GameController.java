998e27ad5a29fd0effd47abc753c5dee91132a41
Joanna <j.k.szczesna@gmail.com>
2024-09-30 10:59:13 +0200

    GameController first test

--- mastermind/src/main/java/pl/joannaszczesna/mastermind/spring/GameController.java
+++ mastermind/src/main/java/pl/joannaszczesna/mastermind/spring/GameController.java
@@ -8,8 +8,13 @@
 package pl.joannaszczesna.mastermind.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.joannaszczesna.mastermind.domain.GameFacade;
+
+import java.util.List;
 
 @RestController
 class GameController {
@@ -18,5 +23,10 @@ class GameController {
     @Autowired
     public GameController(GameFacade gameFacade) {
         this.gameFacade = gameFacade;
+    }
+
+    @PostMapping("/guess-code")
+    ResponseEntity<GameFacade.GameResponse> guessCode(@RequestBody List<Integer> userCode) {
+        return ResponseEntity.ok(null);
     }
 }
--- mastermind/src/test/java/pl/joannaszczesna/mastermind/spring/GameControllerTest.java
+++ mastermind/src/test/java/pl/joannaszczesna/mastermind/spring/GameControllerTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import pl.joannaszczesna.mastermind.domain.CodeGeneratorMock;
+import pl.joannaszczesna.mastermind.domain.GameFacade;
 
 import java.util.List;
 
@@ -22,7 +24,11 @@ import static io.restassured.RestAssured.given;
 class GameControllerTest {
     private static String baseUri;
     private static final String GUESS_CODE_ENDPOINT_PATH = "/guess-code";
-    private static List<Integer> VALID_LENGTH_USER_CODE = List.of(11111);
+    private static List<Integer> VALID_LENGTH_USER_CODE = List.of(22222);
+    private static List<Integer> VALID_CODE = List.of(11111);
+    private static GameFacade GAME_FACADE;
+
+
 
     @BeforeAll
     static void setUp(@LocalServerPort int port) {
@@ -30,19 +36,12 @@ class GameControllerTest {
         RestAssured.port = port;
 
         baseUri = "%s:%d".formatted(RestAssured.baseURI, port);
-    }
-
-    private static String makeGuessCodeRequestBody(List<Integer> code) {
-        return """
-                {
-                "%d"
-                }
-                """.formatted(code);
+        GAME_FACADE = new GameFacade(new CodeGeneratorMock(VALID_CODE));
     }
 
     @Test
     void guessCode_whenSuccessfulOperation_returnStatus200() {
-        given().body(makeGuessCodeRequestBody(VALID_LENGTH_USER_CODE))
+        given().body(VALID_LENGTH_USER_CODE)
                 .contentType(ContentType.JSON)
                 .when()
                 .post(GUESS_CODE_ENDPOINT_PATH)
